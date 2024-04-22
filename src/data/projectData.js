876cc014dc738a4c3f9d10a0329a79b3e4e8a5b2
Rohan-1-3 <rohanshrestha130@gmail.com>
2024-04-22 19:27:51 +0545

    added few more projects

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -9,20 +9,21 @@ export const projectData = [
     },
     {
         id: 2,
-        name: "Calculator",
-        description : "A simple calculator capable of  performing basic arithmetic operations.",
-        image: "https://res.cloudinary.com/fetch-images/image/upload/f_auto/c_crop,g_auto,h_300,w_400/portfolioTemp/calculator.jpg",
-        srcCode: "https://github.com/Rohan-1-3/calculator",
-        liveDemo : "https://rohan-1-3.github.io/calculator/"
+        name: "Pokemon Quiz",
+        description : "A simple game where you have to click all the cards without clicking the same card twice.",
+        image: "https://res.cloudinary.com/fetch-images/image/upload/f_auto/c_crop,g_auto,h_300,w_400/portfolioTemp/pokemon-game.jpg",
+        srcCode: "https://github.com/Rohan-1-3/memory-game",
+        liveDemo : "https://rohan-1-3.github.io/memory-game/"
     },
     {
         id: 3,
-        name: "Calculator",
+        name: "CV Builder",
         description : "A simple calculator capable of  performing basic arithmetic operations.",
-        image: "https://res.cloudinary.com/fetch-images/image/upload/f_auto/c_crop,g_auto,h_300,w_400/portfolioTemp/calculator.jpg",
-        srcCode: "https://github.com/Rohan-1-3/calculator",
-        liveDemo : "https://rohan-1-3.github.io/calculator/"
-    },{
+        image: "https://res.cloudinary.com/fetch-images/image/upload/f_auto/c_crop,g_north_west,h_300,w_400/portfolioTemp/cv-builder.jpg",
+        srcCode: "https://github.com/Rohan-1-3/CV-Builder",
+        liveDemo : "https://resplendent-nougat-40ea32.netlify.app/"
+    },
+    {
         id: 4,
         name: "Calculator",
         description : "A simple calculator capable of  performing basic arithmetic operations.",
@@ -32,20 +33,18 @@ export const projectData = [
     },
     {
         id: 5,
-        name: "Calculator",
-        description : "A simple calculator capable of  performing basic arithmetic operations.",
-        image: "https://res.cloudinary.com/fetch-images/image/upload/f_auto/c_crop,g_auto,h_300,w_400/portfolioTemp/calculator.jpg",
-        srcCode: "https://github.com/Rohan-1-3/calculator",
-        liveDemo : "https://rohan-1-3.github.io/calculator/"
+        name: "Pokemon Quiz",
+        description : "A simple game where you have to click all the cards without clicking the same card twice.",
+        image: "https://res.cloudinary.com/fetch-images/image/upload/f_auto/c_crop,g_auto,h_300,w_400/portfolioTemp/pokemon-game.jpg",
+        srcCode: "https://github.com/Rohan-1-3/memory-game",
+        liveDemo : "https://rohan-1-3.github.io/memory-game/"
     },
     {
         id: 6,
-        name: "Calculator",
+        name: "CV Builder",
         description : "A simple calculator capable of  performing basic arithmetic operations.",
-        image: "https://res.cloudinary.com/fetch-images/image/upload/f_auto/c_crop,g_auto,h_300,w_400/portfolioTemp/calculator.jpg",
-        srcCode: "https://github.com/Rohan-1-3/calculator",
-        liveDemo : "https://rohan-1-3.github.io/calculator/"
+        image: "https://res.cloudinary.com/fetch-images/image/upload/f_auto/c_crop,g_north_west,h_300,w_400/portfolioTemp/cv-builder.jpg",
+        srcCode: "https://github.com/Rohan-1-3/CV-Builder",
+        liveDemo : "https://resplendent-nougat-40ea32.netlify.app/"
     },
-
-
 ]
